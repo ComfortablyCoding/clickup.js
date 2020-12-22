7afdd505a331e2e897a419f4b83a401970e66143
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2020-12-21 23:02:53 -0500

    feat(addAttachment): add fileSettings param
    - increase ease of use
    - it is a required param
    
    BREAKING CHANGE
    pathToFile and fileName have been condensed into one param. To migrate change your projects to use the supported fileSettings param.

--- src/routes/Tasks.js
+++ src/routes/Tasks.js
@@ -56,15 +56,28 @@ class Tasks {
 	 * Add an attachment to a task
 	 *
 	 * @param {String} taskId The task id
-	 * @param {String} pathToFile The path to the file
-	 * @param {String} [fileName='attachment'] The file name
+	 * @param {Object} fileSettings The file settings
+	 * @param {String} fileSettings.filePath The path to the file
+	 * @param {String} fileSettings.fileName The name of the attachment file along with its extension type. Example: 'notes.txt'
 	 * @param {Object} [options] The parameter options to pass in
 	 */
-	async addAttachment(taskId, pathToFile, fileName = 'attachment', options) {
+	async addAttachment(taskId, fileSettings, options) {
+		// ensure fileSettings are provided
+		if (fileSettings) {
+			if (!fileSettings.filePath) {
+				throw new Error('A file path must be provided');
+			}
+			if (!fileSettings.fileName) {
+				throw new Error('A file name must be provided');
+			}
+		} else {
+			throw new Error('File settings must be provided');
+		}
+
 		// building form-data
 		const form = new FormData();
-		form.append('filename', fileName);
-		form.append('attachment', createReadStream(pathToFile));
+		form.append('filename', fileSettings.fileName);
+		form.append('attachment', createReadStream(fileSettings.filePath));
 
 		// setting headers
 		const headers = form.getHeaders();
