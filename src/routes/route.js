6f3a5f6f73c82b0003fe474ddcb3b35c364ef82f
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2024-05-27 03:50:07 -0400

    chore(route): update typing for client

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ export class Route {
 	/**
 	 * The main route for the collection
 	 * @param {object} options
-	 * @param {Clickup} options.client The clickup client instance
+	 * @param {import('../client').Clickup} options.client The clickup client instance
 	 * @param {string} options.route The route path
 	 */
 	constructor(options) {
