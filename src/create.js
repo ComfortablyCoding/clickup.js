d6a39150320688ed62f45fcac580c58917169a75
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2025-11-04 18:19:35 -0500

    add jsdoc for createClickup

--- src/create.js
+++ src/create.js
@@ -1,5 +1,13 @@
 import { Clickup } from "./client.js";
 
-export function createClickup(options) {
-	return new Clickup(options ?? {});
+/**
+ * @param {object} [options] Clickup Constructor options
+ * @param {string} [options.token] Clickup Access Token
+ * @param {object} [options.request] The request options
+ * @param {string} [options.request.prefixUrl=https://api.clickup.com/api] The clickup API URL
+ * @param {string} [options.rateLimit] The clickup API URL
+ * @param {object} [options.hooks] The hooks for extending functionality *
+ */
+export function createClickup(options = {}) {
+	return new Clickup(options);
 }
